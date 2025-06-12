6e17ae2336173a7bda91424aca4c7414c2374c41
EmerBV <emersmc@hotmail.com>
2025-06-12 14:48:12 +0200

    implementing user tiers

--- src/main/java/com/figrclub/figrclubdb/enums/SubscriptionType.java
+++ src/main/java/com/figrclub/figrclubdb/enums/SubscriptionType.java
@@ -1,9 +1,6 @@
 package com.figrclub.figrclubdb.enums;
 
 public enum SubscriptionType {
-    FREE,          // Gratis
-    BASIC,         // Básica
-    PREMIUM,       // Premium
-    PRO,           // Profesional
-    ENTERPRISE     // Empresa
+    FREE,
+    PRO
 }
--- src/main/java/com/figrclub/figrclubdb/enums/UserType.java
+++ src/main/java/com/figrclub/figrclubdb/enums/UserType.java
@@ -2,6 +2,5 @@ package com.figrclub.figrclubdb.enums;
 
 public enum UserType {
     INDIVIDUAL,    // Usuario individual
-    BUSINESS,      // Tienda/Negocio
     PRO_SELLER     // Vendedor profesional
 }
